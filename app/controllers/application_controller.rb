fd4f830f57335b5d3e26e417a011b27571407839
Nicole D <nnardella@gmail.com>
2019-02-02 03:43:31 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,20 +7,10 @@ class App < Sinatra::Base
           erb :team
         end
 
-    post '/teams' do
-
-    @team = Team.new(name: params[:team][:name], motto: params[:team][:motto])
-
-    @heros = params[:team][:member].map do |hero_data|
-           hero = Member.new(hero_data)
-            hero.team = @team
-            hero.save
-            hero
-         end
+        post "/teams" do
+          @team = params["team"]
 
           erb :super_hero
-       end
-
-
+        end
 
     end
